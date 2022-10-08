55b461eade700be9803b73460c15b8fc4b073acf
vallat <vox.crit2013@gmail.com>
2022-10-08 15:19:01 +0400

    locks shuffle and check for win condition

--- formMain.cs
+++ formMain.cs
@@ -18,6 +18,7 @@ namespace DevGridContol
 
         bool[,] cell_states = null;
 
+        int ARRAY_SIZE = 5;
         public formMain()
         {
 
@@ -26,8 +27,6 @@ namespace DevGridContol
 
         private void formMain_Load(object sender, EventArgs e)
         {
-            int ARRAY_SIZE = 20;
-
             InitializeDataGrid(ARRAY_SIZE);
         }
 
@@ -56,16 +55,27 @@ namespace DevGridContol
                 for (int j = 0; j < grid_size; j++)
                 {
                     DataGridViewImageCell cell = (DataGridViewImageCell)DataGrid.Rows[j].Cells[i];
-                    cell.ChangeImage(closed_state);
-                    cell_states[i, j] = false;
+                    cell.ChangeImage(opened_state);
+                    cell_states[i, j] = true;
                 }
             }
+            RandomizeLocks(grid_size / 2, grid_size);
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             //Console.WriteLine(e.ColumnIndex.ToString() + ":" + e.RowIndex.ToString());
             ChangeLocks(e.ColumnIndex, e.RowIndex);
+            Console.WriteLine(CheckWinCondition().ToString());
+        }
+
+        private void RandomizeLocks(int number_of_randoms, int max_random)
+        {
+            Random random = new Random();
+            for (int i = 0; i < number_of_randoms; i++)
+            {
+                ChangeLocks(random.Next(0, max_random), random.Next(0, max_random));
+            }
         }
 
         private void ChangeLocks(int column, int row)
@@ -83,6 +93,28 @@ namespace DevGridContol
             }
         }
         
+
+        private bool CheckWinCondition()
+        {
+            bool win_condition = true;
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                for (int j = 0; j < ARRAY_SIZE; j++)
+                {
+                    win_condition &= cell_states[i, j];
+                    if (!win_condition)
+                    {
+                        break;
+                    }
+                }
+                if (!win_condition)
+                {
+                    break;
+                }
+            }
+            return win_condition;
+        }
+
         private void SwitchCellImage(DataGridViewImageCell cell, int cell_x, int cell_y)
         {
             bool cell_state = cell_states[cell_x, cell_y];
